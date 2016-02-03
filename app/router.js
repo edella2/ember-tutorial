5991657f01bdb31067ee3250d583892153491712
Eric DellAringa <edella2@gmail.com>
2016-02-03 15:43:23 -0800

    finish tutorial. ember-data not showing up.  Need to play with it more

--- app/router.js
+++ app/router.js
@@ -5,8 +5,10 @@ const Router = Ember.Router.extend({
   location: config.locationType
 });
 
+//When generating a route ember maps to it automatically
 Router.map(function() {
   this.route('about');
+  this.route('contact');
 });
 
 export default Router;
